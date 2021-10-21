386a8d9573056b3d71dd6c3d50bc49d7f0372de5
Evgeny Leshevich <evgeniy.le99@gmail.com>
2021-10-21 11:59:56 +0300

    Initial commit

---
+++ src/main/java/com/evgeniy/spring/boot/db/rest/ExampleSpringBootJPARestCRUD/ExampleSpringBootJpaRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.evgeniy.spring.boot.db.rest.ExampleSpringBootJPARestCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootJpaRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootJpaRestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evgeniy/spring/boot/db/rest/ExampleSpringBootJPARestCRUD/ExampleSpringBootJpaRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evgeniy.spring.boot.db.rest.ExampleSpringBootJPARestCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootJpaRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
